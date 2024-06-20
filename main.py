723fb94bda539593aabde707b29fc687dbbb63be
hvosdt <hvosdt@gmail.com>
2024-06-20 15:29:49 +0500

    fix

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ UPLOAD_DIR = 'uploads'
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.post('/upload/{lead_id}')
-async def upload_file(lead_id: int, file: UploadFile = File(...), authorization: str=None):
-    if authorization != 'Bearer Asdf2121':
-        return HTTPException(status_code=401, detail='Unauthorized')
+async def upload_file(lead_id: int, file: UploadFile = File(...)):
+    #if authorization != 'Bearer Asdf2121':
+    #    return HTTPException(status_code=401, detail='Unauthorized')
     
     file_path = os.path.join(UPLOAD_DIR, f'{lead_id}_{file.filename}')
     with open(file_path, 'wb') as f:
