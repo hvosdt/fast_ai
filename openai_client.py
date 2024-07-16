02e3524da46bef3a47424f0b96dedb0dd1d88038
hvosdt <hvosdt@gmail.com>
2024-07-16 17:26:11 +0500

    d

--- openai_client.py
+++ openai_client.py
@@ -5,7 +5,7 @@ def get_recommendations(text):
     client = OpenAI(project='proj_N4qPODMfw3cTgDsgKdeF3fcR', api_key=config.OPENAI_SECRET_KEY)
 
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "Ты аналитик отдела продаж. Дай рекомендации по этому диалогу с клиентом"},
             {"role": "user", "content": text}
