fe2f326d384c60b34423fa4e6af01a79dffa5b0c
hvosdt <hvosdt@gmail.com>
2024-07-17 18:49:31 +0500

    f

--- aws_textract.py
+++ aws_textract.py
@@ -1,22 +1,21 @@
+import textractcaller as tc
+import trp.trp2 as t2
 import boto3
-from textractcaller.t_call import call_textract, Textract_Features, Query, QueriesConfig, Adapter, AdaptersConfig
 
-if __name__ == "__main__":
-    #textract = TextractWrapper(boto3.client("textract"), boto3.resource("s3"), sqs_resource='sqs')
-    #response = textract.analyze_file(['FORMS'], "passport.jpeg")
-    client = boto3.client('textract')
-    with open('image.jpeg', 'rb') as file:
-        document = file.read()
-    # Create AdaptersConfig
-    adapter1 = Adapter(adapter_id="111111111", version=1, pages=[])
-    adapters_config = AdaptersConfig(adapters=[adapter1])
-
-    # Call AnalyzeDocument API with custom adapter
-    response = call_textract(
-        features=[Textract_Features.QUERIES],
-        queries_config=QueriesConfig(adapters_config=adapters_config),
-        document_location={
-                'Bytes': document,                        
-            }
-        )        
-    print(response)
+textract = boto3.client('textract', region_name="us-east-2")
+q1 = tc.Query(text="What is Passport No?", alias="passport_no", pages=["1"])
+q2 = tc.Query(text="What is Surname", alias="surname", pages=["1"])
+adapter1 = tc.Adapter(adapter_id="f63bc34524f2", version="3", pages=["1"])
+textract_json = tc.call_textract(
+    input_document="image.jpeg",
+    queries_config=tc.QueriesConfig(queries=[q1, q2]),
+    adapters_config=tc.AdaptersConfig(adapters=[adapter1]),
+    features=[tc.Textract_Features.QUERIES],
+    force_async_api=True,
+    boto3_textract_client=textract)
+t_doc: t2.TDocument = t2.TDocumentSchema().load(textract_json)  # type: ignore
+for page in t_doc.pages:
+    query_answers = t_doc.get_query_answers(page=page)
+    for x in query_answers:
+        print(f"{x[1]},{x[2]}")
+    
