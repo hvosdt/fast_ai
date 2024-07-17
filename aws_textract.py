78134fd353b503101aed047f5766c9d5ee114571
hvosdt <hvosdt@gmail.com>
2024-07-17 18:41:06 +0500

    f

--- aws_textract.py
+++ aws_textract.py
@@ -1,4 +1,5 @@
 import boto3
+from textractcaller.t_call import call_textract, Textract_Features, Query, QueriesConfig, Adapter, AdaptersConfig
 
 if __name__ == "__main__":
     #textract = TextractWrapper(boto3.client("textract"), boto3.resource("s3"), sqs_resource='sqs')
@@ -6,25 +7,16 @@ if __name__ == "__main__":
     client = boto3.client('textract')
     with open('image.jpeg', 'rb') as file:
         document = file.read()
-            
-    response = client.analyze_document(
-        Document={
-            'Bytes': document,                
-        },
-        FeatureTypes=[
-            'TABLES',
-        ],            
-        AdaptersConfig={
-            'Adapters': [
-                {
-                    'AdapterId': 'f63bc34524f2',                    
-                    'Version': 3
-                },
-            ]
-        }
-    )
+    # Create AdaptersConfig
+    adapter1 = Adapter(adapter_id="111111111", version=1, pages=[])
+    adapters_config = AdaptersConfig(adapters=[adapter1])
+
+    # Call AnalyzeDocument API with custom adapter
+    response = call_textract(
+        features=[Textract_Features.QUERIES],
+        queries_config=QueriesConfig(adapters_config=adapters_config),
+        document_location={
+                'Bytes': document,                        
+            }
+        )        
     print(response)
-    
-    
-    
-    
