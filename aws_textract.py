a151d154caee2aa908506952f64a73678771725a
hvosdt <hvosdt@gmail.com>
2024-07-17 18:12:57 +0500

    textract

---
+++ aws_textract.py
@@ -0,0 +1,25 @@
+import boto3
+
+if __name__ == "__main__":
+    #textract = TextractWrapper(boto3.client("textract"), boto3.resource("s3"), sqs_resource='sqs')
+    #response = textract.analyze_file(['FORMS'], "passport.jpeg")
+    client = boto3.client('textract')
+    with open('image.jpeg', 'rb') as file:
+            
+        response = client.analyze_document(
+            Document={
+                'Bytes': file,                
+            },
+            FeatureTypes=[
+                'QUERIES',
+            ],            
+            AdaptersConfig={
+                'Adapters': [
+                    {
+                        'AdapterId': 'f63bc34524f2',                    
+                        'Version': 'Ver. 3'
+                    },
+                ]
+            }
+        )
+        print(response)
