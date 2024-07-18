b4665616133bfea187e733254429e5da359212d6
hvosdt <hvosdt@gmail.com>
2024-07-18 15:26:50 +0500

    f

--- aws_textract.py
+++ aws_textract.py
@@ -10,10 +10,11 @@ def recognize_passport(filename):
     q4 = tc.Query(text="What is Date of birth", alias="date_of_birth", pages=["1"])
     q5 = tc.Query(text="What is Date of issue", alias="date_of_issue", pages=["1"])
     q6 = tc.Query(text="What is Date of expiry", alias="date_of_expiry", pages=["1"])
+    q7 = tc.Query(text="What is Nationality", alias="nationality", pages=["1"])
     adapter1 = tc.Adapter(adapter_id="f63bc34524f2", version="3", pages=["1"])
     textract_json = tc.call_textract(
         input_document="image.jpeg",
-        queries_config=tc.QueriesConfig(queries=[q1, q2, q3, q4, q5, q6]),
+        queries_config=tc.QueriesConfig(queries=[q1, q2, q3, q4, q5, q6, q7]),
         adapters_config=tc.AdaptersConfig(adapters=[adapter1]),
         features=[tc.Textract_Features.QUERIES],
         boto3_textract_client=textract)
